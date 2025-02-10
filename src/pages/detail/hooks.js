5752a7c8dfa651a8910e01aa5bde51042d2a10a1
Reza Subagja <rezasubagja5@gmail.com>
2025-02-10 11:35:27 +0700

    fix(detail): bug-fix on delete note handler

--- src/pages/detail/hooks.js
+++ src/pages/detail/hooks.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { archiveNote, getNote, showDeleteNoteConfirm, unarchiveNote } from "../../utils/notes";
+import { archiveNote, deleteNote, getNote, showDeleteNoteConfirm, unarchiveNote } from "../../utils/notes";
 import { showErrorAlert } from "../../utils";
 
 function useDetails() {
